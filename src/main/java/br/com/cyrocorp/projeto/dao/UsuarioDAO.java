38778c5cbf7eee7c322b77d80e0214b174a1d6ba
cyroag <cyraoag@hotmail.com>
2020-06-26 11:59:54 -0300

    Adicionando DAO e Controller

---
+++ src/main/java/br/com/cyrocorp/projeto/dao/UsuarioDAO.java
@@ -0,0 +1,12 @@
+package br.com.cyrocorp.projeto.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.cyrocorp.projeto.model.Usuario;
+
+public interface UsuarioDAO extends CrudRepository<Usuario, Integer>{
+	
+	public Usuario findByEmailAndSenha(String email, String senha);
+	public Usuario findByRacfAndSenha(String racf, String senha);
+
+}
